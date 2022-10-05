61a90dc8f9a000ff8721919f5c446d543fd1a4cd
darahta <mehmedimsm@hotmail.com>
2022-10-05 20:22:55 +0300

    alısveris-sitesi

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
+function App() {
+   return (
+      <div className="App">
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={<ProductListing />} />
+               <Route path="/product/:productId" element={<ProductDetail />} />
+               <Route>404 Not Found</Route>
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
+}
+
+export default App;
